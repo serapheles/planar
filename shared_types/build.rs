79852fef528ad1ce7ec45ea743df5dad26172f1e
serapheles <katathis@gmail.com>
2024-04-25 16:59:36 -0700

    Adding files

---
+++ shared_types/build.rs
@@ -0,0 +1,21 @@
+use crux_core::typegen::TypeGen;
+use shared::{Planar};
+use std::path::PathBuf;
+
+fn main() -> anyhow::Result<()> {
+    println!("cargo:rerun-if-changed=../shared");
+
+    let mut gen = TypeGen::new();
+
+    gen.register_app::<Planar>()?;
+
+    let output_root = PathBuf::from("./generated");
+
+    gen.swift("SharedTypes", output_root.join("swift"))?;
+
+    gen.java("com.example.planar.shared_types", output_root.join("java"))?;
+
+    gen.typescript("shared_types", output_root.join("typescript"))?;
+
+    Ok(())
+}
